cb7b27fefcfd1762121f1bf9c73efeece1e1c75b
TheMr00gibs <hugodacostapina@icloud.com>
2024-06-23 22:24:57 +0200

    🎉 Start project

--- Banking System/main.cpp
+++ Banking System/main.cpp
@@ -4,6 +4,7 @@
 //
 //  Created by Hugo Da Costa Pina on 23/06/2024.
 //
+/* A C++ project is designed with an object-oriented approach, with classes for customers, accounts, transactions, and banking services.Features included creating and managing customers, accounts, and transactions, as well as performing banking services such as withdrawals, deposits, and transfers. It also allows customers to view their account information, including account balances, recent transactions, and other details.Technologies Required: C++ programming language, OOPS, and MySQL. */
 
 #include <iostream>
 
