c02ba925f7ff2f8a791770f243f6ed82d24d0b88
TheMr00gibs <hugodacostapina@icloud.com>
2024-06-23 21:34:00 +0200

    Initial Commit

---
+++ Banking System/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Banking System
+//
+//  Created by Hugo Da Costa Pina on 23/06/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
